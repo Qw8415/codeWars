84a63dda33263deda64ea498d5c0d692c46252bd
Qw8415 <g.szul00@gmail.com>
2019-08-10 17:02:59 +0200

    Add IntervalTest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,11 @@
 import kyu4.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[][] array = new int[][] {{1,2}, {3,4}};
-        for (int[] i : array) {
-            System.out.println();
-            for (int j : i) {
-                System.out.print(j + "\t");
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.println();
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + "\t");
-            }
-        }
     }
-
-
 
 }
---
+++ src/main/java/kyu4/Interval.java
@@ -0,0 +1,39 @@
+package kyu4;
+
+
+import java.util.Arrays;
+
+public class Interval {
+    public static int sumIntervals(int[][] intervals) {
+        sort(intervals);
+        mergeOverlappingIntervals(intervals);
+        int sum = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            sum += intervals[i][1] - intervals[i][0];
+        }
+        return sum;
+    }
+
+    private static void mergeOverlappingIntervals(int[][] arr) {
+        for (int i = 1, k = 0; i < arr.length; k = i, i++) {
+            if (arr[i][0] < arr[k][1]) {
+                arr[i][0] =  arr[k][0];
+                arr[i][1] = (arr[i][1] > arr[k][1]) ? arr[i][1] : arr[k][1];
+                arr[k][0] = arr[k][1] = 0;
+            }
+        }
+    }
+
+    private static void sort(int[][] arr) {
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 1; j < arr.length - i; j++)
+                if (arr[j - 1][0] > arr[j][0])
+                    swap(arr, j, j-1);
+    }
+
+    private static void swap(int[][] arr, int i, int j) {
+        int[] temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
